00d5321b75c90f139157ebde876f4624efcc39ec
Anish-Programmer <at7anish@gmail.com>
2024-06-23 09:36:49 +0545

    Using JlLabel for lblTitle + Font for lblTitle font

--- PetRecordSystem.java
+++ PetRecordSystem.java
@@ -1,13 +1,23 @@
 package w_18_java_assignment;
 //importing packages and class
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
 public class PetRecordSystem{
 
     //instance variables
-    private JFrame frame;
+    private JFrame frame; //Jframe instance variable
+    private JLabel lblTitle; //JLabel  : title
 
     //constructor
     public PetRecordSystem(){
+
+
+        //creating object of JLabel : lblTitle
+        lblTitle = new JLabel();
+        lblTitle.setText("Pet Record System"); //lblTitle : text
+        lblTitle.setBounds(310,50,200,30); //lblTitle : location in JFrame
+        lblTitle.setFont(new Font("Arial",Font.BOLD,15)); //lblTitle : setting font size
 
 
         //creating obect of JFrame
@@ -16,6 +26,8 @@ public class PetRecordSystem{
         frame.setLayout(null);
         frame.setBounds(300,150,750,650);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(lblTitle); //adding lblTitle :in frame
 
 
 
